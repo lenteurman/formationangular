7535b7c294b8193dd7bc9484e27d60d90e68753b
CHABRAT <donovan.chabrat.external@atos.net>
2017-11-06 14:38:54 +0100

    Initialisation

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name formationApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the formationApp
+ */
+angular.module('formationApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
